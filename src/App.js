983bffb6f0be42cc03ffc46534cc6d5b5fb3101c
Eduardo W. da Cunha <eduardowestc@gmail.com>
2022-04-28 18:37:23 -0300

    added heeey

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Home from './components/Home';
 
 function App() {
 
-let hello = "hello world"
+  let hello = "hello world"
 
-console.log(hello + "bing bong bang boom")
+  console.log(hello + "bing bong bang boom heeey")
 
-let food = "hamburger"
+  let food = "hamburger"
 
   return (
 
     <>
- <h2>
- {hello}
- </h2>
+      <h2>
+        {hello}
+      </h2>
 
-<Home data = {hello} data2 = {food}/>
-<AdminMinting/>
+      <Home data={hello} data2={food} />
+      <AdminMinting />
 
 
     </>
