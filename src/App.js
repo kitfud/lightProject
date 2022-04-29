21be6584d4aafa81b9ade4b2e2d15aee48dac35d
Eduardo W. da Cunha <eduardowestc@gmail.com>
2022-04-29 18:39:35 -0300

    worked on the Admin (NFT Minting) page and cleaned the index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
-import './App.css';
 import AdminMinting from './components/AdminMinting';
 import Home from './components/Home';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Shop from './components/Shop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import {createTheme, ThemeProvider, Card, Container, CardContent} from '@mui/material'
+import { createTheme, ThemeProvider, Card, Container, CardContent } from '@mui/material'
 
 
 let theme = createTheme({
@@ -30,22 +29,22 @@ function App() {
   return (
 
     <>
-   
-   <ThemeProvider theme={theme}>
 
-   <Header/>
-    <Card sx={{height:'100vh',backgroundColor:'#5357bf'}}>
-    <Routes>
-    <Route path='/' element ={<Home/>}/>
-    <Route path='/home' element ={<Home/>}/>
-    <Route path='/shop' element ={<Shop/>}/>
-    <Route path ='/admin' element = {<AdminMinting/>}/>     
-    </Routes>
-    </Card>
-    <Footer/>
+      <ThemeProvider theme={theme}>
+
+        <Header />
+        <Card sx={{ height: '100vh', backgroundColor: '#5357bf' }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/shop' element={<Shop />} />
+            <Route path='/admin' element={<AdminMinting />} />
+          </Routes>
+        </Card>
+        <Footer />
 
 
-   </ThemeProvider>
+      </ThemeProvider>
 
 
 
