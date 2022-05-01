5d26cca0f286ac48c2f527f866fe616914a62290
Svarogg <svarogg@tutanota.com>
2022-05-01 19:31:33 +0200

    - Added the depoyed LightFactory contract address in public/ - Added the solidity contracts in src/contracts - Added the contracts ABIs in src/ABIs/

---
+++ src/contracts/LightFactory.sol
@@ -0,0 +1,105 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "./LightGenerator.sol";
+
+// TO DO: change checkIfTokenHolder to a mapping with boolean?
+// TO DO: change the onlyTokenOwner modifier
+// TO DO: change to onlyOwner to Ownable
+// TO DO: change to several owner - whitelist?
+// TO DO: implement VRFV2 for NFTs rarity?
+// TO DO:
+// TO DO:
+
+contract LightFactory is ERC721URIStorage, Ownable {
+
+    string public NFT_URI = "REPLACE BY IMAGES UPLOADED ON IPFS";
+
+    uint256 public currentNFTPriceInUSD;
+    uint256 public tokenCount;
+
+    AggregatorV3Interface internal ETHUSDPriceFeed;
+    address priceFeedAddress; // pass to the Light Generator constructor when deployed
+
+    mapping(uint256 => LightGenerator) public tokenIDToGenerator;
+    // mapping(address => bool) public addressOwnsToken; // could be useful
+
+
+    constructor(address _priceFeedAddress) ERC721('Light Maker', 'VIBE') {
+        // ethusd price feed address on rinkeby : 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        priceFeedAddress = _priceFeedAddress;
+        ETHUSDPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        currentNFTPriceInUSD = 100 * 10**18;
+        tokenCount = 0;
+    }
+
+    function setNFTPrice(uint256 price) public onlyOwner {
+        currentNFTPriceInUSD = price;
+    }
+
+    // get NFT price in ETH
+    function getNFTPriceInETH() public view returns(uint256) {
+        uint256 precision = 1 * 10**18;
+        return (currentNFTPriceInUSD * precision)/getETHUSDConversionRate();
+    }
+
+    function getETHUSDConversionRate() public view returns(uint256) {
+        (,int256 price,,,) = ETHUSDPriceFeed.latestRoundData();
+        uint256 factor = 18 - ETHUSDPriceFeed.decimals();
+        return uint256(price)* 10**factor;
+    }
+
+    function mintGenerator() public payable {
+        require(msg.value >= getNFTPriceInETH(),"Not Enough Eth to purchase NFT.");
+        (bool sent,) = owner().call{value:msg.value}("");
+        require(sent, "Failed to purchase NFT. Send Eth to buy.");
+
+        uint256 newTokenId = tokenCount;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, NFT_URI);
+        LightGenerator generator = new LightGenerator(
+            this,
+            tokenCount,
+            priceFeedAddress
+        );
+        tokenIDToGenerator[tokenCount] = generator;
+        tokenCount++;
+    }
+
+    function checkTokenOwnerById(uint tokenId) public view returns(address) {
+        return ownerOf(tokenId);
+    }
+
+
+    function checkIfTokenHolder(address toSearch) public view returns(bool) {
+        for (uint i = 1; i<=tokenCount;i++){
+            if(checkTokenOwnerById(i)== toSearch){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // check if a boolean mapping could work
+    function addressToTokenID(address toSearch) public view returns(uint256 [] memory){
+         uint256 [] memory values = new uint256[](tokenCount);
+         uint256 j;
+         for (uint i = 1; i<=tokenCount;i++){
+            if(checkTokenOwnerById(i)== toSearch){
+                values[j]=i;
+                j++;
+            }
+        }
+        return values;
+    }
+
+    function getGeneratorContractAddressByToken(uint256 tokenId) public view returns (address){
+        return tokenIDToGenerator[tokenId].getAddress();
+    }
+
+}
---
+++ src/contracts/LightGenerator.sol
@@ -0,0 +1,128 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import "./LightFactory.sol";
+
+// TO DO: decide if the products will only be local or a contract
+// TO DO: add the pricefeed and a way to pay more than the receive function.
+
+contract LightGenerator {
+
+    LightFactory public factory;
+    uint256 public tokenId;
+    uint256 public productCount;
+    // add a product object
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 priceUSD;
+    }
+
+    AggregatorV3Interface internal ETHUSDPriceFeed;
+    Product[] public products; // products history, record of all products ever added until reinitialized
+    mapping(uint256 => Product) public idToProduct; // keep in mind mappings cannot be deleted in solidity
+    mapping(string => Product) public nameToProduct;
+    // mapping(uint256 => bool) public productAvailable;
+    event ProductSold(
+        uint256 indexed _id,
+        address buyer,
+        uint256 price,
+        uint256 timestamp
+    );
+    event ProductAdded(
+        uint256 indexed _id,
+        string name,
+        uint256 timestamp
+    );
+    event Deposit(address indexed payee, uint256 value, uint256 time, uint256 currentContractBalance);
+    event Withdraw(uint256 time, uint256 amount, address indexed owner);
+
+    constructor (LightFactory _factory, uint256 id, address _priceFeedAddress) payable {
+        // ethusd price feed address on rinkeby : 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        ETHUSDPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        factory = _factory;
+        tokenId = id;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == factory.ownerOf(tokenId), "function is only for owner");
+        _;
+    }
+
+    function getBalance() public view onlyOwner returns(uint256 balance){
+        balance = address(this).balance;
+    }
+
+    function withdraw() public onlyOwner() {
+        uint256 contractBalance = address(this).balance;
+        address owner = factory.ownerOf(tokenId);
+        (bool sent,) = owner.call{value:address(this).balance}("");
+        require(sent,"Failed to Send Ether To Owner");
+        emit Withdraw(block.timestamp, contractBalance, owner);
+    }
+
+    function getAddress() public view returns (address){
+        return address(this);
+    }
+
+    function getProductPriceInETH(uint256 productId) public view returns(uint256) {
+        uint256 precision = 1 * 10**18;
+        uint256 productPriceinUSD = idToProduct[productId].priceUSD;
+        return (productPriceinUSD * precision)/getETHUSDConversionRate();
+    }
+
+    function getETHUSDConversionRate() public view returns(uint256) {
+        (,int256 price,,,) = ETHUSDPriceFeed.latestRoundData();
+        uint256 factor = 18 - ETHUSDPriceFeed.decimals();
+        return uint256(price)* 10**factor; 
+    }
+
+    function changeProductPrice(uint256 _id, uint256 _priceUSD) external onlyOwner {
+        Product memory currentProduct = idToProduct[_id];
+        Product memory modProduct = Product(
+            currentProduct.id,
+            currentProduct.name,
+            _priceUSD
+        );
+        products.push(modProduct);
+        idToProduct[currentProduct.id] = modProduct;
+        nameToProduct[currentProduct.name] = modProduct;
+    }
+
+    function buyProduct(uint256 productId) payable external {
+        uint256 priceETH = getProductPriceInETH(productId);
+        require(msg.value >= priceETH, "Not Enough ETH to purchase the product.");
+        emit Deposit(msg.sender, msg.value, block.timestamp, address(this).balance);
+        emit ProductSold(productId, msg.sender, priceETH, block.timestamp);
+    }
+
+    function fund() payable external {
+        emit Deposit(msg.sender, msg.value,block.timestamp, address(this).balance);
+    }
+
+    function addProduct(string memory _name, uint256 _price) public onlyOwner {
+        Product memory newProduct = Product(productCount, _name, _price);
+        products.push(newProduct);
+        idToProduct[productCount] = newProduct;
+        nameToProduct[_name] = newProduct;
+        emit ProductAdded(productCount, _name, block.timestamp);
+        productCount++ ;
+    }
+
+    function reinitializeProductsHistory() public onlyOwner {
+        // Will cost a lot of gas if the product list is large, careful
+        delete products;
+        for (uint256 i ; i<productCount ; i++) {
+            Product memory prod = idToProduct[i];
+            string memory prodName = prod.name;
+            delete idToProduct[i];
+            delete nameToProduct[prodName];
+        }
+        productCount = 0;
+    }
+
+    receive() external payable{
+        emit Deposit(msg.sender, msg.value,block.timestamp, address(this).balance);
+    }
+}
