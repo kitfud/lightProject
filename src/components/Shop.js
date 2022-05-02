e6f229f61d8e21f571b672c2ba2ab0667714a5f7
Kit Fuderich <kitfuderich@Kits-MacBook-Pro.local>
2022-05-02 14:56:55 -0700

    demo code added for study

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button} from '@mui/material'
 
 const Shop = () => {
+
+const [num, setNum] = useState(0)
+
+const changeNum = () =>{
+let number = num
+number++
+setNum(number)
+}
+
+function myName(){
+    return "Kit"
+}
+
+let user = myName()
+console.log(user)
+
+
+const MyAnimal = () =>{
+    return(
+        <div>
+            <Button sx={{color:"pink"}} variant="contained" >Animal Button</Button>
+        </div>
+    )
+}
+
+const MyDinner = () =>{
+    return(
+        <div>Apple Pie</div>
+    )
+}
+
+
+1==="1" ? console.log("true"): console.log("false")
+
+
   return (
-    <div>Shop</div>
+<>
+<div>{`my number is ${num}`}</div>
+<button onClick={changeNum}>Change Num</button>
+{
+    num%2==0?(
+<MyAnimal/>
+    ):
+    (
+<MyDinner/>
+    )
+}
+
+
+</>
   )
+
 }
 
 export default Shop
