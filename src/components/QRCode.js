23ad0e77a0648bf0d72747e7dd29187637f7b896
GZRdev <GZRdev@github.com>
2022-05-02 15:53:45 -0700

    Added QRCode.js, updated Home.js colors, get qr button, and added qr image.

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import pic from '../img/Sample_QR_code.png'
 import {
     Box,
@@ -7,50 +7,115 @@ import {
     Container,
     IconButton,
     Card,
-    Grid
-  } from '@mui/material';
+    Grid,
+    Modal
+  } from '@mui/material'
+import { elementAcceptingRef } from '@mui/utils'
+
+
 
 const QRCode = () => {
+/*
+    const [open, setOpen] = useState(false)
+
+    const handleClose = () => {
+      setOpen(false)
+    }
+*/
 
     const howToQR = "Select a color, then click the button below."
-
     const [showQR, setShowQR] = useState(false)
 
-    const handleClick = (e, showQR) => {
+    const handleClick = (e) => {
+    
+
+      if (showQR === true) {
+        setShowQR(false)
+      }
+
       console.log('hello', e.target)
+      
+      setShowQR(true)
 
-      let qR = showQR ? pic : null
-
-      //document.createElement("img")
-      //img.src = pic
-      //document.body.appendChild(img)
-      /*
-      const img = new Image(200,200) //width, height
-      img.src = pic
-      img.id = "QR"
-      document.getElementById("QR")
-      */
     }
 
+
+    /*
+    <Button onClick={handleOpen}>Open modal</Button>
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Text in a modal
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+    </Typography>
+  </Box>
+</Modal>
+    */
+  
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+
   return (
-    <div className="qRCode">
-      <div className="content">
-        <div className="explainQR">
-          <center><h4>{ howToQR }</h4></center>
-        </div>
-        <Button
-            className="qRButton" 
-            size="large"
-            variant='contained'
-            id="buttonQR"
-            onClick={ (e) => { handleClick(e, setShowQR(true)) } }>
-            Get New QR Code
-        </Button>
-        <br/><br/>
-        {/*<div className="qRPic">
-          <img src={ require=(pic) } alt="QR Code" width="200" height="200"/>
-  </div>*/}
-      </div>
+    // <div className="qRCode">
+    //   <div className="content">
+    //     <div className="explainQR">
+    //       <center><h4>{ howToQR }</h4></center>
+    //     </div>
+    //     <Button
+    //         className="qRButton" 
+    //         size="large"
+    //         variant='contained'
+    //         id="buttonQR"
+    //         onClick={ (e) => { handleClick(e) } }>
+    //         Get New QR Code
+    //     </Button>
+    //     <Modal
+    //       open={open}
+    //       onClose={handleClose}
+    //       aria-labelledby="modal-modal-title"
+    //       aria-describedby="modal-modal-description">
+    //       <Box>
+    //         <Typography id="modal-modal-title" variant="h6" component="h2">
+    //         Text in a modal
+    //         </Typography>
+    //         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+    //           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+    //         </Typography>
+    //       </Box>
+    //     </Modal>
+    //     <br/><br/>
+    //     <div className="qRPic">
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <center>
+
+            <img src={pic} height="200" width="200" margintop="300px"/>
+          </center>
+        </Box>
+      </Modal>
     </div>
   )
 }
