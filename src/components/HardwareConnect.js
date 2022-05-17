99b77f7f26864f91a57abd106c32e2b6ec386f3b
Kit Fuderich <kitfuderich@Kits-MacBook-Pro.local>
2022-05-17 10:59:40 -0700

    local changes saved

--- src/components/HardwareConnect.js
+++ src/components/HardwareConnect.js
@@ -14,18 +14,9 @@ const HardwareConnect = ({ handleAlerts }) => {
 
   const handleConnect = async () => {
     if ("serial" in navigator) {
-<<<<<<< HEAD
-      const new_port = await navigator.serial.requestPort()
-      console.log("in Hardware Connect", new_port)
-      console.log("New port: ", new_port)
-      dispatch(setPort(new_port))
-      const res= await new_port.open({ baudRate: 57600 })
-      console.log(res)
-=======
       serialRef.current = connect(57600)
       dispatch(setPort(serialRef.current))
  
->>>>>>> 8dc7f65 (hardware lights kind of working, connect listner in app.js with triggering send function in home.js)
     } else {
       handleAlerts("Web serial API is not supported by the browser", "warning")
     }
