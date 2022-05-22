860c76fe7a261d959cdba4cfe49c9120680b7910
Svarogg <svarogg@tutanota.com>
2022-05-23 01:14:14 +0200

    comment out unused function in the interfaces/contracts

--- src/contracts/ILightAgora.sol
+++ src/contracts/ILightAgora.sol
@@ -5,6 +5,6 @@ interface ILightAgora {
 
     function mintTokens(address generatorAddress) external;
 
-    function receivePayment(uint256 _amount) external payable;
+    // function receivePayment(uint256 _amount) external payable;
 
 }
